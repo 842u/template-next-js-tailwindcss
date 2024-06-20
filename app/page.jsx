aff68f89f0aa0ec7e09149a67d5dcf47e8abdccd
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-06-20 17:06:10 +0200

    chore: initial project setup

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1>🫡</h1>
+    </main>
+  );
+}
